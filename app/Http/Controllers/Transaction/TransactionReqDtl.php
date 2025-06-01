d7907662bfc98d50f064c9982e19beeb2a512ee3
dzikra <dzikra355@gmail.com>
2025-06-01 16:20:29 +0700

    menambahkan service baru beserta model dan controller untuk get list issue by requestor dan dtl

--- app/Http/Controllers/Transaction/TransactionReqDtl.php
+++ app/Http/Controllers/Transaction/TransactionReqDtl.php
@@ -2,35 +2,43 @@
 
 namespace App\Http\Controllers\Transaction;
 
-
 use App\Models\TransRequestor;
 use Illuminate\Http\Request;
 
-class TransactionRequestor 
+class TransactionReqDtl
 {
     public function index(Request $request)
     {
         $createdBy = $request->input('created_by_id');
 
-        if (!$createdBy) {
+        if(!$createdBy){
             return response()->json([
                 'success' => false,
                 'message' => 'created by id is required'
             ]);
         }
 
-        $getList = TransRequestor::where('created_by_id', $createdBy)->get();
+        $issueId = $request->input('issueid');
 
-        if ($getList->isEmpty()) {
+        if(!$issueId){
             return response()->json([
                 'success' => false,
-                'message' => 'Data not found'
+                'message' => 'issue id is required'
+            ]);
+        }
+
+        $getListDtl = TransRequestor::where('created_by_id', $createdBy)->where('issueid', $issueId)->get();
+
+        if($getListDtl->isEmpty()){
+            return response()->json([
+                'success'=> false,
+                'message'=> 'Data not found'
             ]);
         }
 
         return response()->json([
-            'success' => true,
-            'data' => $getList
+            'success'=> true,
+            'data'=> $getListDtl
         ]);
     }
 }
